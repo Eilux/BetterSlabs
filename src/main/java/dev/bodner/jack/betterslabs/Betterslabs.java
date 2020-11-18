b5864e5ee649ad60a5a2095b7eae2576f3df8bb1
Jack Bodner <jack@bodnerfamily.com>
2020-11-18 16:34:42 -0500

    all bugs fixed... for now....

--- src/main/java/dev/bodner/jack/betterslabs/Betterslabs.java
+++ src/main/java/dev/bodner/jack/betterslabs/Betterslabs.java
@@ -1,16 +1,22 @@
 package dev.bodner.jack.betterslabs;
 
 import dev.bodner.jack.betterslabs.block.SlabBlockMod;
+import dev.bodner.jack.betterslabs.client.BetterslabsClient;
 import dev.bodner.jack.betterslabs.component.Components;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import virtuoel.statement.api.StateRefresher;
 
 public class Betterslabs implements ModInitializer {
 
@@ -27,6 +33,15 @@ public class Betterslabs implements ModInitializer {
                         Components.MODE_KEY.get(packetContext.getPlayer()).incrementPlaceMode();
                         Components.MODE_KEY.sync(packetContext.getPlayer());
                     });
-                });
+        });
+
+        for (final Identifier id : Registry.BLOCK.getIds()){
+            final Block entry = Registry.BLOCK.get(id);
+            if (entry.getClass() == SlabBlock.class || entry.getClass().getSuperclass() == SlabBlock.class){
+                BetterslabsClient.slabList.add(id);
+                StateRefresher.INSTANCE.addBlockProperty(entry, Properties.AXIS, Direction.Axis.Y);
+                StateRefresher.INSTANCE.reorderBlockStates();
+            }
+        }
     }
 }
